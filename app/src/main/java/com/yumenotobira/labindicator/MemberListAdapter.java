7e897eb72e214f281d9768a18633e3bd4307766f
yumenotobira <grnggm+yumenotobira@gmail.com>
2015-07-23 21:27:57 +0900

    Add indicator images

--- app/src/main/java/com/yumenotobira/labindicator/MemberListAdapter.java
+++ app/src/main/java/com/yumenotobira/labindicator/MemberListAdapter.java
@@ -1,6 +1,7 @@
 package com.yumenotobira.labindicator;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,26 +15,31 @@ import java.util.List;
  */
 public class MemberListAdapter extends ArrayAdapter<User> {
     private LayoutInflater inflater;
+    private Resources resources;
 
     public MemberListAdapter(Context context, int resource, List<User> objects) {
         super(context, resource, objects);
         inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        resources = context.getResources();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = inflater.inflate(R.layout.member_list, null, false);
         TextView memberNameView = (TextView)view.findViewById(R.id.member_name_item);
-        TextView memberPresenceView = (TextView)view.findViewById(R.id.member_presence_item);
         TextView memberMemoView = (TextView)view.findViewById(R.id.member_memo_item);
+        TextView memberPresenceTextView = (TextView)view.findViewById(R.id.member_presence_text);
+        TextView memberAbsenceTextView = (TextView)view.findViewById(R.id.member_absence_text);
 
         User member = getItem(position);
         memberNameView.setText(member.name);
 
         if(member.presence) {
-            memberPresenceView.setText("在室");
+            memberPresenceTextView.setBackgroundColor(resources.getColor(R.color.indicator_background));
+            memberAbsenceTextView.setTextColor(resources.getColor(R.color.no_indicator_textColor));
         } else {
-            memberPresenceView.setText("不在");
+            memberAbsenceTextView.setBackgroundColor(resources.getColor(R.color.indicator_background));
+            memberPresenceTextView.setTextColor(resources.getColor(R.color.no_indicator_textColor));
         }
 
         memberMemoView.setText(member.memo);
